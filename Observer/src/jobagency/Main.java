0347e8b10e475c071bfa0d474ad8530aad2a28a6
Georgvit <ifreyd@mail.ru>
2023-01-20 23:17:35 +0300

    observer

--- Observer/src/jobagency/Main.java
+++ Observer/src/jobagency/Main.java
@@ -9,8 +9,10 @@ public class Main {
         Company geekBrains = new Company(jobAgency, "GeekBrains", 100);
         Student petrov = new Student("Petrov");
         Master ivanov = new Master("Ivanov");
+        PnD professor = new PnD("Вася");
         jobAgency.registerObserver(petrov);
         jobAgency.registerObserver(ivanov);
+        jobAgency.registerObserver(professor);
 
         for(int i = 0; i< 10; i++){
             google.needEmpoyee();
--- Observer/src/jobagency/Master.java
+++ Observer/src/jobagency/Master.java
@@ -12,10 +12,10 @@ public class Master implements Observer {
     @Override
     public void receiveOffer(String nameCompany, int salary) {
         if (salary < 80){
-            System.out.println(String.format("Master %s: I am waiting for the better job! (company, salary) = %s, %d", name, nameCompany, salary));
+            System.out.println(String.format("Master %s: Я жду лучшей работы! (company, salary) = %s, %d", name, nameCompany, salary));
         }
         else{
-            System.out.println(String.format("Master %s: Ok. I am ready for this job! (company, salary) = %s, %d", name, nameCompany, salary));
+            System.out.println(String.format("Master %s: Хорошо. Я готов к этой работе! (company, salary) = %s, %d", name, nameCompany, salary));
         }
     }
 }
